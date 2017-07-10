d83cd5e571e9290c8b01c2a16a297a0116d94655
Tim <ycg166911@163.com>
2017-07-10 16:51:57 +0800

    创建发送邮件方法，添加配置文件

--- src/common_util.py
+++ src/common_util.py
@@ -1,3 +1,8 @@
+import smtplib, traceback
+from email.mime.text import MIMEText
+
+import settings
+
 class Entity():
     def __init__(self):
         pass
@@ -8,3 +13,29 @@ def get_object(row):
         setattr(info, key, value)
     return info
 
+def send_text(subject, to_list, content):
+    send_mail(subject, to_list, content, "plain")
+
+def send_html(subject, to_list, content):
+    send_mail(subject, to_list, content, "html")
+
+def send_mail(subject, to_list, content, mail_type):
+    list_t = []
+    server = None
+    if(isinstance(to_list, list) == False):
+        list_t.append(to_list)
+    try:
+        message = MIMEText(content, _subtype=mail_type, _charset="utf8")
+        message['Subject'] = subject
+        message['To'] = ";".join(list_t)
+        message['From'] = settings.EMAIL_USER
+
+        server = smtplib.SMTP()
+        server.connect(settings.EMAIL_HOST)
+        server.login(settings.EMAIL_USER, settings.EMAIL_PASSWORD)
+        server.sendmail(settings.EMAIL_USER, list_t, message.as_string())
+    except:
+        traceback.print_exc()
+    finally:
+        if(server != None):
+            server.close()
