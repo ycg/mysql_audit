0f40f4f7f41075d6579a41eae3a9a75df9f94b87
Tim <ycg166911@163.com>
2017-08-11 17:42:55 +0800

    简单优化了一下集群主机管理的界面

--- src/host_manager.py
+++ src/host_manager.py
@@ -3,13 +3,15 @@
 import traceback, json
 import settings, cache, db_util, common_util
 
+
 def query_host_infos(obj):
-    result = cache.MyCache().get_mysql_host_info(host_id=obj.host_id)
-    if(obj.host_type > 0):
+    return cache.MyCache().get_mysql_host_info()
+    '''result = cache.MyCache().get_mysql_host_info(host_id=obj.host_id)
+    if (obj.host_type > 0):
         pass
-    if(len(obj.host_name) > 0):
+    if (len(obj.host_name) > 0):
         pass
-    return result
+    return result'''
 
 
 def add(obj):
@@ -20,15 +22,18 @@ def add(obj):
     db_util.DBUtil().execute(settings.MySQL_HOST, sql)
     cache.MyCache().load_mysql_host_infos()
 
+
 def update(obj):
     sql = "select ip, port, `user`, `password`, host_name from mysql_audit.mysql_hosts where host_id = {0}".format(obj.host_id)
     info = common_util.get_object(db_util.DBUtil().fetchone(settings.MySQL_HOST, sql))
     return json.dumps(info, default=lambda o: o.__dict__)
 
+
 def delete(obj):
     sql = "update mysql_audit.mysql_hosts set is_deleted = 1 where host_id = {0};".format(obj.host_id)
     db_util.DBUtil().execute(settings.MySQL_HOST, sql)
     cache.MyCache().load_mysql_host_infos()
+
 
 def test_connection(obj):
     try:
@@ -37,6 +42,7 @@ def test_connection(obj):
         traceback.print_exc()
         return "连接失败!"
     return "连接成功!"
+
 
 def get_host_info(obj):
     sql = "select ip, port, `user`, `password`, host_name from mysql_audit.mysql_hosts where host_id = {0}".format(obj.host_id)
